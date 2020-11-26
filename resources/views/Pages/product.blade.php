7ff29aea9fddfede2c6896226c7a0226a6ae3d2e
Kevin Tabaraka <66250033+XKevinTX@users.noreply.github.com>
2020-11-26 16:58:13 +0700

    Product update (desc w/ button)

--- resources/views/Pages/product.blade.php
+++ resources/views/Pages/product.blade.php
@@ -7,14 +7,19 @@
 
 @section('content')
 <div class="container content-container">
-    <h1>Lorem Ipsum</h1>
+    <h2><br></h2>
     <div class="row">
         <div class="col-4">
             <img class="card-img"
                 src="https://static.shop.adidas.co.id/media/catalog/product/cache/2/thumbnail/1200x/9df78eab33525d08d6e5fb8d27136e95/E/G/EG4958_SL_eCom.jpg">
         </div>
         <div class="col-8">
-
+            <h2>Adidus Superstar Shoes</h5>
+            <p>Rp. 1000000</p>
+            <p>Description:<br>Good Quality Shoes!</p>
+            @if (auth()->user())
+                <button type="button" class="btn btn-primary">Add to Cart</button>
+            @endif
         </div>
     </div>
 </div>
