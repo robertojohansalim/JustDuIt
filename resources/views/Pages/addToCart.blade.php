780cdf8811f76ad26fb013ceaa593039826d7661
Kevin Tabaraka <66250033+XKevinTX@users.noreply.github.com>
2020-12-03 21:25:26 +0700

    Little update

--- resources/views/Pages/addToCart.blade.php
+++ resources/views/Pages/addToCart.blade.php
@@ -17,25 +17,25 @@
         </div>
         <div class="col-8">
             <h2>{{ $shoe->name }}</h5>
-                <p>Rp. {{ number_format($shoe->price) }}</p>
-                <p>{{ $shoe->description }}</p>
-                {{-- Form --}}
-                <form action="{{ route('addToCart', ['id'=>$shoe->id]) }}" method="post">
-                    {{ csrf_field() }}
-                    <input type="hidden" name="product_id" value="{{ $shoe->id }}">
-                    <div class="qty">
-                        <div class="form-group row d-flex justify-content" style="margin-top: 40px">
-                            <label for="qtyForm" class="col-sm-1 col-form-label text-left"
-                                style="margin-right: 40px">Quantity</label>
-                            <button type="button" class="minus"><i class="fas fa-minus"></i></button>
-                            <input class="quantity" min="0" name="quantity" value="1" type="number"
-                                style="width: 50px; text-align: center">
-                            <button type="button" class="plus"><i class="fas fa-plus"></i></button>
-                        </div>
-                        <button type="submit" class="btn btn-primary" style="margin-top: 10px">Add to Cart</button>
+            <p>Rp. {{ number_format($shoe->price) }}</p>
+            <p>{{ $shoe->description }}</p>
+            {{-- Form --}}
+            <form action="{{ route('addToCart', ['id'=>$shoe->id]) }}" method="post">
+                {{ csrf_field() }}
+                <input type="hidden" name="product_id" value="{{ $shoe->id }}">
+                <div class="qty">
+                    <div class="form-group row d-flex justify-content" style="margin-top: 40px">
+                        <label for="qtyForm" class="col-sm-1 col-form-label text-left"
+                            style="margin-right: 40px">Quantity</label>
+                        <button type="button" class="minus"><i class="fas fa-minus"></i></button>
+                        <input class="quantity" min="0" name="quantity" value="1" type="number"
+                           style="width: 50px; text-align: center">
+                        <button type="button" class="plus"><i class="fas fa-plus"></i></button>
                     </div>
-                </form>
-                {{-- END of Form --}}
+                    <button type="submit" class="btn btn-primary" style="margin-top: 10px">Add to Cart</button>
+                </div>
+            </form>
+            {{-- END of Form --}}
         </div>
     </div>
     <p><br></p>
