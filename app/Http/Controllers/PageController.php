17c5bd06f582c6a189375518836dde3f9abbbe31
Kevin Tabaraka <66250033+XKevinTX@users.noreply.github.com>
2020-12-04 17:11:36 +0700

    Fix cart page when 'empty for 1st time'

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -19,12 +19,29 @@ class PageController extends Controller
         return view('Pages/home', $data);
     }
 
-    public function login()
+    public function login(Request $request)
     {
+        if($request->isMethod("POST")){
+            if(LoginController::authenticate($request)){
+                return redirect()->intended(url()->route('dashboard'));
+            }
+            return back()->withInput()->withErrors(['msg'=>'login fail, check your email or password']);
+        }
         return view('Pages/login');
     }
 
-    public function register(){
+    public function register(Request $request){
+        if($request->isMethod("POST")){
+            // Validating Input
+            $errors = RegisterController::register($request);
+            if(!$errors){
+                // Accepted
+                return redirect()->route('dashboard');
+            }
+            else{
+                redirect()->route('register')->withInput()->withErrors($errors);
+            }
+        }
         return view('Pages/register');
     }
 
@@ -76,11 +93,20 @@ class PageController extends Controller
 
     public function cart(){
         $cart = Auth()->user()->cart;
-        $data = [
-            'cart' => $cart,
-            'cart_items' => $cart->items,
-        ];
-        return view('Pages/cart', $data);
+        if($cart){
+            $data = [
+                'cart' => $cart,
+                'cart_items' => $cart->items
+            ];
+            return view('Pages/cart', $data);
+        }
+        else{
+            $data = [
+                'cart' => null,
+                'cart_items' => null
+            ];
+            return view('Pages/cart', $data);
+        }
     }
 
     public function transaction(){
