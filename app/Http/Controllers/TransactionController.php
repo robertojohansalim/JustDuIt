05b9ed86002308a7933b0f93a638920d1a4986e4
Roberto Johan Salim <69228888+robertojohansalim@users.noreply.github.com>
2020-12-05 16:59:01 +0700

    Add Empty Cart Checkout Validation on Controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -9,12 +9,16 @@ use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
-    public static function makeTransaction($cartItems){
+    public static function makeTransaction($cartItems)
+    {
+        if ($cartItems == null || count($cartItems) == 0) {
+            return null;
+        }
         $user = Auth::user();
         $date = now();
         $header = TransactionHeader::makeTransactionHeader($user->id, $date);
         $header->id;
-        foreach($cartItems as $item){
+        foreach ($cartItems as $item) {
             TransactionDetail::makeTransactionDetail($header->id, $item->shoe->id, $item->shoe->image, $item->shoe->price, $item->quantity);
         }
         return $header;
